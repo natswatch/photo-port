1a08e7ce171d8a20b67cd1430ccb96a077da8fbc
natswatch <nathaliec@me.com>
2020-12-11 13:12:54 -0800

    form and submit handlers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState }from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactFrom from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -24,10 +25,9 @@ function App() {
         currentCategory={currentCategory}
         ></Nav>
       <main>
-        <div>
-          <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        <ContactFrom></ContactFrom>
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About> 
       </main>
     </div>
   );
