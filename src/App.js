a820a8f8638e14892b3bf650439f13d4ad757e3c
natswatch <nathaliec@me.com>
2020-12-11 15:51:36 -0800

    conditionals for contact

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Gallery from './components/Gallery';
 import ContactFrom from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     {
       name: 'commercial',
@@ -23,11 +24,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
         ></Nav>
       <main>
-        <ContactFrom></ContactFrom>
-        <Gallery currentCategory={currentCategory}></Gallery>
-        <About></About> 
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About> 
+          </>
+        ) : (
+          <ContactFrom></ContactFrom>
+        )}
       </main>
     </div>
   );
